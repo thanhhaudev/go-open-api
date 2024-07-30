1ca20f3f2262ae2373bc4158bfc35cebb96ea181
thanhhaudev <thanhhaudev@gmail.com>
2024-07-30 15:29:51 +0700

    add swaggo

--- main.go
+++ main.go
@@ -5,16 +5,22 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
-
-func YourHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Gorilla!\n"))
-}
 
 func main() {
 	r := mux.NewRouter()
-	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", YourHandler)
+
+	r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8080/swagger/doc.json"),
+		httpSwagger.DeepLinking(true),
+		httpSwagger.DocExpansion("none"),
+		httpSwagger.DomID("swagger-ui"),
+	)).Methods(http.MethodGet)
+
+	r.HandleFunc("/api/health", func(w http.ResponseWriter, request *http.Request) {
+		w.Write([]byte("Gorilla!\n"))
+	})
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":3000", r))
